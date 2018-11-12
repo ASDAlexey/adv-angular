f882a9eb8adca190b4be1dfdb634c58e26f083df
Alexey Popov <asdalexey@yandex.ru>
2018-11-12 10:24:49 +0300

    add injectors

--- lesson-2-di/di/src/app/app.module.ts
+++ lesson-2-di/di/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NgZone, ReflectiveInjector } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserService } from './user.service';
-import { LogService } from './log.service';
 import { ChildComponent } from './child/child.component';
+import { NewComponent } from './new/new.component';
+import { LogService } from './log.service';
+
+const injector: ReflectiveInjector = ReflectiveInjector.resolveAndCreate([UserService]);
+const childInjector = injector.resolveAndCreateChild([UserService]);
+
+// injector !== childInjector
+// injector.get(UserService) !== injector.get(childInjector)
+
+// Platform, Compiler, NgZone - ReflectiveInjector => StaticInjector
+// Module, Component - not ReflectiveInjector
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +40,8 @@ import { ChildComponent } from './child/child.component';
 
     // { provide: AdminService, useClass: AdminService },
     // { provide: UserService, useExisting: AdminService } // использовать UserService как AdminService но по interface не со всему методами
-    UserService,
+    // UserService,
+    // { provide: UserService, useClass: UserService, deps: [LogService] } // StaticInjector - чуть быстрее работает
   ],
   bootstrap: [AppComponent]
 })
--- lesson-2-di/di/src/app/child/child.component.ts
+++ lesson-2-di/di/src/app/child/child.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserService } from '../user.service';
+import { LogService } from '../log.service';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
   providers: [UserService],
+  viewProviders: [LogService], // доступ только у самого компонента но не у компонентов ставленных через ng-container <app-child><<app-new></app-child>
 })
 export class ChildComponent {
   constructor(public userService: UserService) {
