b8002ed211ba03e20d5d222636645014b6777310
Alexey Popov <asdalexey@yandex.ru>
2018-11-24 00:39:25 +0300

    add routing animation

--- lesson5-ngcli/router/src/app/app-routing.module.ts
+++ lesson5-ngcli/router/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MyPreload } from './lazy/my-preload';
 
+// https://stackblitz.com/edit/angular-router-fade-transitions-msnhib?file=app%2Fanimations%2Ffade.animation.ts
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule', data: { nopreload: true } },
