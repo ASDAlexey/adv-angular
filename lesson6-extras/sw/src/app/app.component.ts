ec7fb46eefd2b04dec12d8146a9432cf7d8edafb
Alexey Popov <asdalexey@yandex.ru>
2018-11-24 23:32:21 +0300

    add detect sw updated

--- lesson6-extras/sw/src/app/app.component.ts
+++ lesson6-extras/sw/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'sw';
+  title = 'sw title nnn';
+
+  constructor(private updates: SwUpdate) {
+    this.updates.available.subscribe(() => {
+      alert('App was updated');
+    });
+  }
 }
